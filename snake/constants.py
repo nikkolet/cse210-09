4587806f22ffcd73443be202cedd8b03827ca30d
nikkolet <nikkoletashby@Gmail.com>
2022-06-24 23:38:12 -0600

    commit

--- snake/constants.py
+++ snake/constants.py
@@ -8,7 +8,7 @@ MAX_X = 900
 MAX_Y = 600
 FRAME_RATE = 15
 FONT_SIZE = 15
-CAPTION = "Snake"
+CAPTION = "SNAKE"
 SNAKE_LENGTH = 8
 WHITE = Color(255, 255, 255)
 RED = Color(255, 0, 0)
@@ -16,6 +16,7 @@ YELLOW = Color(255, 255, 0)
 GREEN = Color(0, 255, 0)
 ORANGE = Color(255,165,0)
 AQUA = Color(0, 255, 255)
-Colors = [RED, YELLOW, GREEN, ORANGE, AQUA]
+BLUE = (0,0,255)
+Colors = [RED, YELLOW, GREEN, ORANGE, AQUA,BLUE]
 Color = random.choice(Colors)
 
